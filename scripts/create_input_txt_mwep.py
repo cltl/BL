fc2c6f54098fbfa5bb91c3cc3dd899a1da1669b6
MartenPostma <martenp@gmail.com>
2020-05-22 13:55:40 +0200

    subsumed by

--- scripts/create_input_txt_mwep.py
+++ scripts/create_input_txt_mwep.py
@@ -2,11 +2,12 @@
 Create a txt file with all event types to be run with MWEP
 
 Usage:
-  create_input_txt_mwep.py --path_ev_coll_obj=<path_ev_coll_obj> --output_path=<output_path> --path_config_json=<path_config_json> --verbose=<verbose>
+  create_input_txt_mwep.py --path_ev_coll_obj=<path_ev_coll_obj> --output_path=<output_path> --path_config_json=<path_config_json> --path_mwep_json=<path_mwep_json> --verbose=<verbose>
 
 Options:
     --path_ev_coll_obj=<path_ev_coll_obj> path to pickled EventTypeCollection class
     --path_config_json=<path_config_json> e.g., ../config/v0.json
+    --path_mwep_json=<path_mwep_json> e.g., ../config/mwep_settings.json
     --output_path=<output_path> path where txt is stored with all the event types to be run with MWEP
     OUTPUT_FOLDER/event_types.txt is stored
     --verbose=<verbose> verbose level
@@ -14,7 +15,8 @@ Options:
 Example:
     python create_input_txt_mwep.py\
      --path_ev_coll_obj="../wd_cache/ev_type_coll.p"\
-     --path_config_json="../config/v0.json"\
+     --path_config_json="../config/v1.json"\
+     --path_mwep_json="../config/mwep_settings.json"\
      --output_path="../wd_cache/event_types.txt"\
      --verbose=2
 """
@@ -33,9 +35,16 @@ print()
 
 verbose = int(arguments['--verbose'])
 settings = json.load(open(arguments['--path_config_json']))
+mwep = json.load(open(arguments['--path_mwep_json']))
 
-ev_coll_obj = pickle.load(open(arguments['--path_ev_coll_obj'], 'rb'))
 
-ev_coll_obj.get_subsumers_of_set_of_event_types(event_types=settings['event_types'],
-                                                output_path=arguments['--output_path'],
-                                                verbose=verbose)
+if mwep['event_type_matching'] == 'direct_matching':
+    ev_coll_obj = pickle.load(open(arguments['--path_ev_coll_obj'], 'rb'))
+
+    ev_coll_obj.get_subsumers_of_set_of_event_types(event_types=settings['event_types'],
+                                                    output_path=arguments['--output_path'],
+                                                    verbose=verbose)
+elif mwep['event_type_matching'] == 'subsumed_by':
+    with open(arguments['--output_path'], 'w') as outfile:
+        for event_type in settings['event_types']:
+            outfile.write(f'{event_type}\n')
--- scripts/data_release.sh
+++ scripts/data_release.sh
@@ -1,33 +1,35 @@
+export LC_ALL="en_US.UTF-8"
+
 bash represent_wd.sh
 
 bash create_input_txt_mwep.sh
 
 python call_mwep.py --path_config_json="../config/v1.json" --verbose="1"
 
-python mwep_integrations.py --path_config_json="../config/v1.json" --verbose="2"
+#python mwep_integrations.py --path_config_json="../config/v1.json" --verbose="2"
 
 # call Open-Sesame
-python run_open_sesame.py --path_config_json="../config/v1.json" --verbose="2"
+#python run_open_sesame.py --path_config_json="../config/v1.json" --verbose="2"
 
 # integrate structured data
-python integrate_structured_data.py --path_config_json="../config/v1.json" --verbose="2"
+#python integrate_structured_data.py --path_config_json="../config/v1.json" --verbose="2"
 
 # add lexicon data
-python add_lexicon_data.py --path_config_json="../config/v1.json" --verbose="2"
+#python add_lexicon_data.py --path_config_json="../config/v1.json" --verbose="2"
 
 # add typical frames
-python ../res/typical_frames/typical_frames.py --path_config_json="../config/v1.json" --verbose="2"
+#python ../res/typical_frames/typical_frames.py --path_config_json="../config/v1.json" --verbose="2"
 
 # add SEM representation
-python convert_to_sem.py --path_config_json="../config/v1.json" --verbose="2"
+#python convert_to_sem.py --path_config_json="../config/v1.json" --verbose="2"
 
 # add JSON version
-python write_structured_and_unstructured.py --path_config_json="../config/v1.json" --verbose="2"
+#python write_structured_and_unstructured.py --path_config_json="../config/v1.json" --verbose="2"
 
 # add descriptive statistics
-python write_stats.py --path_config_json="../config/v1.json" --verbose="2"
+#python write_stats.py --path_config_json="../config/v1.json" --verbose="2"
 
 # add README, LICENSE
-python add_readme_license.py --path_config_json="../config/v1.json" --verbose="2"
+#python add_readme_license.py --path_config_json="../config/v1.json" --verbose="2"
 
 
